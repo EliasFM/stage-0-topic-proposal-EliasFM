af93057d8ac492522e593fb44adae3f24557402b
EliasFM <elias.mendel@gmail.com>
2018-11-28 15:25:16 -0800

    Updates

--- README.md
+++
@@ -1,7 +0,0 @@
-# Course Project
-
-This repository contains code for an interactive web app, created for the _Client-Side Web Development_ course at the UW iSchool.
-
-This will hopefully provide insight into drug overdoses in america, and the areas where people and policy makers should focus their attention.
-
-The site can be viewed at <https://info340-au18.github.io/project-EliasFM/>
---
+++ precache-manifest.36cbb77da0283c8296b9fe6216e3bd91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9ab5c282f9547ac7c924",
+    "url": "/stage-0-topic-proposal-EliasFM/static/js/runtime~main.9ab5c282.js"
+  },
+  {
+    "revision": "e023ab17f56a9224f1e1",
+    "url": "/stage-0-topic-proposal-EliasFM/static/js/main.e023ab17.chunk.js"
+  },
+  {
+    "revision": "05862f7c81add0b85e02",
+    "url": "/stage-0-topic-proposal-EliasFM/static/js/1.05862f7c.chunk.js"
+  },
+  {
+    "revision": "e023ab17f56a9224f1e1",
+    "url": "/stage-0-topic-proposal-EliasFM/static/css/main.baddc3a6.chunk.css"
+  },
+  {
+    "revision": "05862f7c81add0b85e02",
+    "url": "/stage-0-topic-proposal-EliasFM/static/css/1.c033e74b.chunk.css"
+  },
+  {
+    "revision": "8957f737b89b96cd2ee615cb594a2898",
+    "url": "/stage-0-topic-proposal-EliasFM/index.html"
+  }
+];
